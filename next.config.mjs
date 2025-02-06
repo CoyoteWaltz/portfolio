e9d4ecbb6ee360f3f6251b8dcf082e464cca4e94
Coyote <46889283+CoyoteWaltz@users.noreply.github.com>
2025-02-06 17:43:34 +0800

    Update and rename next.config.js to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
-const withNextra = require('nextra')({
+import nextra from 'nextra'
+ 
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true,
 })
 
-module.exports = withNextra({
+export default withNextra({
   // i18n: {
   //   locales: ['en', 'zh'],
   //   defaultLocale: 'en',
